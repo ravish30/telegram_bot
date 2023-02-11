4c22f7f64c14e411f54f7882e5072a80582e54e2
Ravish <=>
2023-02-11 23:09:49 +0530

    update

---
+++ data/Shaayri.js
@@ -0,0 +1,14 @@
+exports.data = [
+    "Magar likhaye koi uss ko khat toh hum se likhwaye huyi subah aur ghar se kaan par rakh kar qalam nikle.",
+    "Isharat-ae-katra hai dariya me fanaa ho jana dard ka hadd se guzarna hai dawa ho jana.",
+    "Kisi ki kya mazal thi jo koi hume kharid sakta hum to khud hi bik gaye Khariddar Dekhakar",
+    "Mohabbat me nahi fark jine aur marne ka, usiko dekhkar jitehai jis kafir pe dam nikle.",
+    "shk par joor nahi hai ye wo aatish ghalib, ki lagaye na lage bujhaye na bujhe.",
+    "Iss saadgi pe kaun na mar jaye a khuda, ladtehai aur haath me talwarbhi nahi",
+    "Hamko malum hai jannat ki hakikat lekin, dil ke khush rahaneko ghalib ye khayal accha hai.",
+    "Tum na aaye toh kya sagar naa huwi, haa magar chain se basar na huwi, mere nala suna jamanene, ek tum ho jise khabar na huwi.",
+    "Hazaroon khwahishe aisi ki har kwahish pe dam nikle, bahut nikle mere aarman lekin firbhi kam nikle.",
+    "Huyi Muddat Ki “Galib” Mar Gaya Par Yaad Aata Hai, Wo Har Ek Baat Pe Kahata Ki Yun Hota To Kya Hota",
+    "Bana Hai Shah Ka Musaahib Fire Hai Itraana, Vagana Shahar Me “Galib” Ki Abaru Kya Hai.",
+    "Ishk mujhko nahi washahat hi sahi, meri washahat teri shohorat hi sahi."
+]
--- index.js
+++ index.js
@@ -1,60 +1,58 @@
-const dotenv = require('dotenv')
-const express = require('express')
-const axios = require('axios')
+const dotenv = require("dotenv");
+const express = require("express");
+const axios = require("axios");
+const {data} = require("./data/Shaayri");
 const app = express();
-dotenv.config()
+dotenv.config();
 
-const { TOKEN, SERVER_URL, PORT } = process.env
+const { TOKEN, SERVER_URL, PORT } = process.env;
 const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
 const URI = `/webhook/${TOKEN}`;
 const WEBHOOK_URL = SERVER_URL + URI;
 
-
-
 app.use(express.json());
 
 const init = async () => {
-    const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`)
-    console.log(res.data)
-}
+  const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`);
+  console.log(res.data);
+};
 
-
-app.get('/', async (req, res) => {
-    res.send('Hello world');
-})
+app.get("/", async (req, res) => {
+  res.send("Hello world");
+});
 
 app.post(URI, async (req, res) => {
-    console.log(req.body)
+  console.log(req.body);
 
-    const chatID = req.body.message.chat.id;
-    const text = req.body.message.text;
+  const chatID = req.body.message.chat.id;
+  const text = req.body.message.text;
 
-    const reply = await axios.get('https://8768zwfurd.execute-api.us-east-1.amazonaws.com/v1/compliments');
+  let msg = "";
+  if (text == "/tareef") {
+    const reply = await axios.get(
+      "https://8768zwfurd.execute-api.us-east-1.amazonaws.com/v1/compliments"
+    );
+    msg += reply.data;
+  } 
+  else if(text == "/shayari") {
+    const num = Math.floor(Math.random()*10);
+    // console.log(num);
+    msg += data[num];
+  }
+  
+  else {
+    msg += "Could not able to understand :(";
+  }
 
+  await axios.post(`${TELEGRAM_API}/sendMessage`, {
+    chat_id: chatID,
+    text: msg,
+  });
 
-    let msg = '';
-    if(text == 'praise') {
-        msg += reply.data;
-    }
-    else {
-        msg += 'type praise'
-    }
-
-
-    
-
-    await axios.post(`${TELEGRAM_API}/sendMessage`, {
-        chat_id: chatID,
-        text: msg
-    })
-
-    return res.send();
-
-})
-
-
+  return res.send();
+});
 
 app.listen(PORT, async () => {
-    console.log('app started at port', PORT);
-    await init();
-})
+  console.log("app started at port", PORT);
+  await init();
+});
